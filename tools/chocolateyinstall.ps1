100ed050c2e550380632775a0b8d714d2df3140b
Bob Tanner <tanner@real-time.com>
2019-04-26 13:53:36 -0500

    Initial import
    Copied Draftsight chocolatey package from
    
    https://github.com/teknowledgist/Chocolatey-packages/tree/master/automatic/draftsight
    
    Putting installer into the package to address
    
    DraftSight 2019 for Windows® is offered in paid versions only.
    
    Once you download and install DraftSight 2019 (free 30-day trial or purchased version), you will no longer be able to redownload or access any previous free version of DraftSight (2018 or earlier).
    
    All free versions of DraftSight (2018 or earlier) will cease to run after 12/31/2019.

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,45 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = 'DraftSight'
+$url         = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2018SP3/DraftSight32.exe'
+$url64       = 'http://dl-ak.solidworks.com/nonsecure/draftsight/2018SP3/DraftSight64.exe'
+$checkSum    = '916ed3d4d5d740d372f53db71676c89e0081b272b1bf541fe3e9b8231e60b619'
+$checkSum64  = 'e17837ebd6a7c062d2502a397308a7a34515462834b23fbc134e686d4b3de195'
+
+# if an older version of DraftSight has been run, the API service will prevent upgrading it.
+if (Get-Service -DisplayName "Draftsight API Service*" | Where {$_.status -eq 'running'}) {
+   Stop-Service -DisplayName "Draftsight API Service*" -Force -PassThru
+}
+
+$WorkSpace = Join-Path $env:TEMP "$packageName.$env:chocolateyPackageVersion"
+
+$WebFileArgs = @{
+   packageName  = $packageName
+   FileFullPath = Join-Path $WorkSpace "$packageName.exe"
+   Url          = $url
+   Url64bit     = $url64
+   Checksum     = $checkSum
+   Checksum64   = $checkSum64
+   ChecksumType = 'sha256'
+   GetOriginalFileName = $true
+}
+
+$PackedInstaller = Get-ChocolateyWebFile @WebFileArgs
+
+$UnzipArgs = @{
+   PackageName  = $packageName
+   FileFullPath = $PackedInstaller
+   Destination  = $WorkSpace
+}
+
+Get-ChocolateyUnzip @UnzipArgs
+
+$InstallArgs = @{
+   PackageName    = $packageName
+   File           = Join-Path $WorkSpace "$packageName.msi"
+   fileType       = 'msi'
+   silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`" ALLUSERS=1"
+   validExitCodes = @(0, 3010, 1641)
+}
+
+Install-ChocolateyInstallPackage @InstallArgs
